dc81a2735335e49deeef076781768e0ae27aa849
guigur <guigur13@gmail.com>
2019-11-20 16:49:14 +0100

    v2

---
+++ Catchphrase.js
@@ -0,0 +1,11 @@
+var catchphrases = [
+    "A nice server",
+    "A cool IP",
+    "Nice and calm",
+    "Fork me on github !",
+    "Now with 2 times more colors",
+    "apt-get install cool-server",
+    "Unicorn line website",
+    "Dev mode for easter eggs",
+
+];
---
+++ Services.js
@@ -0,0 +1,6 @@
+services = { list: [
+        {"name":"guigur.com", "url":"https://guigur.com"},
+        {"name":"jeej.guigur.com", "url":"https://jeej.guigur.com"},
+        {"name":"margoulin.org", "url":"https://margoulin.org"}
+        ]
+};
